2308a1aa90d6be22229bfc33069807ce24394e8e
Gabriel Trindade <52544804+gvtrindade@users.noreply.github.com>
2022-04-25 17:43:42 -0300

    Curso Tratamento de Exceções adicionado

---
+++ Java/Exceptions/src/DivisaoNaoExataException.java
@@ -0,0 +1,10 @@
+public class DivisaoNaoExataException extends Exception{
+    int numerador;
+    int denominador;
+
+    public DivisaoNaoExataException(String message, int numerador, int denominador) {
+        super(message);
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+}
---
+++ Java/Exceptions/src/Main.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numerador = {4, 5, 8, 10};
+        int[] denominador = {2, 4, 0, 2, 8};
+
+        for (int i = 0; i < denominador.length; i++){
+            try{
+                if (numerador[i] % 2 != 0)
+                    throw new DivisaoNaoExataException("Divisão não exata!", numerador[i], denominador[i]);
+                int resultado = numerador[i] / denominador[i];
+                System.out.println(resultado);
+            } catch (DivisaoNaoExataException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            } catch (ArithmeticException e){
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Impossível dividir por zero!");
+            } catch (ArrayIndexOutOfBoundsException e){
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Valor excedente em algum dos arrays!");
+            }
+        }
+
+        System.out.println("O programa continua...");
+    }
+
+}
